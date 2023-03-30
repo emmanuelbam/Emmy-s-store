fbd2f08af470eab0366d86aeb7d2361083aefdb6
bamidele emmanuel <emmanuelbam9876@gmail.com>
2023-03-29 20:36:56 -0700

    i add javascript

---
+++ Emmy's store.js
@@ -0,0 +1,34 @@
+let product = {
+    data: [
+        {
+            productName:"MOSCHINO",
+            category:"Topwear", 
+            price:"$75",
+            image:"topwear img.jpg",
+        },
+        {
+            productName:"adidas black tshirt",
+            category:"Topwear", 
+            price:"$85",
+            image:"addidas tshirt.jpg", 
+        },
+        {
+            productName:"Apple watch series 7",
+            category:"Watch", 
+            price:"$100",
+            image:"apple watch.jpg", 
+        },
+        {
+            productName:"Blue jacket",
+            category:"Jacket", 
+            price:"$85",
+            image:"jacket img.jpg", 
+        },
+        {
+            productName:"White pants",
+            category:"Bottomwear", 
+            price:"$35",
+            image:"bottomwear img.jpg", 
+        },
+    ],
+} 
